1431aae2225560cecce07c493d810c3da92e4088
EAgha <41126467+EAghayev@users.noreply.github.com>
2023-12-14 21:59:10 +0400

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Lesson1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var number = 4; // int
+            //var number = 4.5; //double
+
+            byte number1 = 10; //0 to 255
+            sbyte number2 = -128; //-128 to 127
+
+            short number3 = 3245; // -32,768 to 32,767
+            ushort number4 = 3456; // 0 to  65,535
+
+            int number5 = 235443556;//-2,147,483,648 to 2,147,483,647
+            uint number6 = 3454; //0 to 4,294,967,295
+
+            long number7 = 34453545534353; // -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            ulong number8 = 3455465465654; //0 to 18,446,744,073,709,551,615
+
+            float number9 = 45.6f;
+            double number10 = 45.6;
+            decimal number11 = 45.6m;
+
+            string word = "Salam 302";
+            char letter = 'L';
+
+            bool check = false;
+
+            Console.WriteLine(check);
+            check = 10 > 5;
+
+            Console.WriteLine(check);
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Lesson1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number; //unassigned
+
+            number = 10;
+
+            Console.WriteLine(number);
+
+
+            number = 11;
+
+            Console.WriteLine(number);
+
+
+            number = 456;
+
+            Console.WriteLine(number);
+
+
+            //var result = true;
+
+            //var result = number % 2==0;
+
+            bool result;
+            if (number % 2 == 0)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+
+
+            Console.WriteLine(result);
+
+            int student_count; // snake case
+            int StudentCount; // pascal case
+            int studentCount; // camel case
+            // student-count // kabab case
+
+            int avgPointOfStudents = 14;
+
+            //======================================================================
+            Console.WriteLine("==========================================================");
+
+            int num;
+            num = 45 + 10;
+            num = 45 - 10;
+            num = 45 * 10;
+            num = 45 / 10;
+            num = 45 % 10;
+
+            num = 100;
+
+            int sum = ++num + 10 + num;
+
+            Console.WriteLine(sum);
+
+            num = 10;
+
+            num += 20; // num=num+20
+            num -= 20; // num = num-20;
+            num *= 20; // num = num*20;
+            num /= 20; // num = num/20;
+            num %= 20; // num = num%20;
+
+            num = 10;
+
+            var check = num != 10;//false
+            check = num == 10; //true;
+            check = num >= 10;//true; 
+            check = num <= 10; //true;
+            check = num == 10; //true;
+
+            var newCheck = !(num>20); //true
+
+            num = 9;
+
+            check = num > 10 && num<15;
+            check = num < 10 || num > 15;
+
+            num = 8;
+            check = (num > 10 && num % 3 == 0) || (num < 10 && num % 2 == 0);
+
+        }
+    }
+}
